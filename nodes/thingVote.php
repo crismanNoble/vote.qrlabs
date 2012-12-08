f2fb3ac247fc8f8a9031bdff303e9ebaec5874fe
Cris Noble <cris@crisnoble.com>
2012-12-08 15:25:09 -0600

    pushing the data into votes table

--- nodes/thingVote.php
+++ nodes/thingVote.php
@@ -11,6 +11,7 @@ $user = $_SERVER['HTTP_USER_AGENT'];
 
 //add vote into votes table
 $sql = "INSERT INTO `angelaj2_qrvotes`.`votes` (`id`, `thing`, `time`, `user`) VALUES (NULL, ".$thing." , CURRENT_TIMESTAMP, ".$uesr.");";
+mysql_query($sql);
 
 //change count in things table for given thing id
 
