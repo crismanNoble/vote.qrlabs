a232ea649fc1251718e4795d76733fc391ab3fff
Cris Noble <cris@crisnoble.com>
2012-12-08 15:30:15 -0600

    writing to db

--- nodes/thingVote.php
+++ nodes/thingVote.php
@@ -10,7 +10,7 @@ $thing = $_GET["thing"];
 $user = $_SERVER['HTTP_USER_AGENT'];
 
 //add vote into votes table
-$sql = "INSERT INTO `angelaj2_qrvotes`.`votes` (`id`, `thing`, `time`, `user`) VALUES (NULL, ".$thing." , CURRENT_TIMESTAMP, ".$uesr.");";
+$sql = "INSERT INTO `angelaj2_qrvotes`.`votes` (`id`, `thing`, `time`, `user`) VALUES (NULL, ".$thing." , CURRENT_TIMESTAMP, ".$user.");";
 mysql_query($sql);
 
 //change count in things table for given thing id
