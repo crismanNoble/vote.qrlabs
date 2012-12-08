07d93230b330cd474de6ef9138aa73a0d8e0985b
Cris Noble <cris@crisnoble.com>
2012-12-08 16:14:41 -0600

    adding vote conting functionality

--- nodes/voteCount.php
+++ nodes/voteCount.php
@@ -8,7 +8,7 @@ $thing2 = $_GET["thing2"];
 
 //determine number of votes for the things.
 $result1 = mysql_query("SELECT * FROM `votes`") or die(mysql_error());
-$result2 = mysql_query("SELECT * FROM `votes` WHERE `thing` = $thing2") or die(mysql_error());
+$result2 = mysql_query("SELECT * FROM `votes` WHERE `thing` = '$thing2'") or die(mysql_error());
 
 $count1 = mysql_numrows($result1);
 $count2 = mysql_numrows($result2);
