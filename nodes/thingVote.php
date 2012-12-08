d8256f71f5cab4ffbd1652a9143c31eb78232e6f
Cris Noble <cris@crisnoble.com>
2012-12-08 15:02:29 -0600

    ua string grabage

--- nodes/thingVote.php
+++ nodes/thingVote.php
@@ -5,10 +5,15 @@
 $thing = $_GET["thing"];
 //grab thing direction
 
+//grab info about the user?
+$user = $_SERVER['HTTP_USER_AGENT'];
+
 //add vote into votes table
 
 //change count in things table for given thing id
 
 //return thank you message
 print $thing;
+print '<br/>';
+print $user;
 ?>
