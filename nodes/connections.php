895f88b1e5de57022163d24062885ed3ebc02c1b
Cris Noble <cris@crisnoble.com>
2012-12-08 15:17:23 -0600

    doing sql work

---
+++ nodes/connections.php
@@ -0,0 +1,4 @@
+<?php
+	mysql_connect("localhost","angelaj2_qrVoter","katPoop") or die(mysql_error()); 
+	mysql_select_db("angelaj2_qrvotes") or die(mysql_error());
+?>
--- nodes/thingVote.php
+++ nodes/thingVote.php
@@ -1,5 +1,6 @@
 <?php
 //connect to database
+include 'connections.php';
 
 //Grab thing
 $thing = $_GET["thing"];
@@ -9,7 +10,7 @@ $thing = $_GET["thing"];
 $user = $_SERVER['HTTP_USER_AGENT'];
 
 //add vote into votes table
-
+$sql = "INSERT INTO `angelaj2_qrvotes`.`votes` (`id`, `thing`, `time`, `user`) VALUES (NULL, ".$thing." , CURRENT_TIMESTAMP, ".$uesr.");";
 //change count in things table for given thing id
 
 //return thank you message
