d0e8ab992972114f48e7ae66812250695dc2bfd1
Cris Noble <cris@crisnoble.com>
2012-12-08 15:18:49 -0600

    connections

--- nodes/connections.php
+++ nodes/connections.php
@@ -1,4 +1,4 @@
 <?php
-	mysql_connect("localhost","angelaj2_qrVoter","katPoop") or die(mysql_error()); 
+	mysql_connect("localhost","angelaj2_qrvoter","katPoop") or die(mysql_error()); 
 	mysql_select_db("angelaj2_qrvotes") or die(mysql_error());
 ?>
