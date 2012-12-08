ea720d13eb74943dfc6440fada22d5465cc1046b
Cris Noble <cris@crisnoble.com>
2012-12-08 15:52:15 -0600

    cleaning up APW imports

--- nodes/thingVote.php
+++ nodes/thingVote.php
@@ -1,6 +1,6 @@
 <?php
 //connect to database
-include_once 'APW_functions.php';
+//include_once 'APW_functions.php';
 include_once 'connections.php';
 $connection = APW_Prepare_DB();
 
@@ -13,7 +13,7 @@ $user = $_SERVER['HTTP_USER_AGENT'];
 
 //add vote into votes table
 $sql = "INSERT INTO `angelaj2_qrvotes`.`votes` (`thing`, `user`) VALUES ('$thing', '$user');";
-$sql = APW_DB_Prepare_String($sql);
+//$sql = APW_DB_Prepare_String($sql);
 
 $query = mysql_query($sql) or die(mysql_error());
 
